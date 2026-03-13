bf4a5fae309e437754b63476357740fb113a90fb
Mohammad Khan <mokhan@virt-3325.kl.dfki.de>
2026-03-13 15:48:40 +0100

    minor updates

--- assets/js/nurbs.js
+++ assets/js/nurbs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cb6efe78decea6b811dc84085c74c08e6cc3f91723eea9ac3b77e82b58c1819
-size 6010
+oid sha256:0f72ab8f546a8901f16bb08151825107da687950125263cfe3737e5acce3ba70
+size 6009
